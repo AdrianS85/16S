62f7a3ea6478614a322c0867af1d3a01b95b5f48
AdrianS85 <adrianstankiewicz85@gmail.com>
2020-11-04 16:22:10 +0100

    awesome_boobies

--- deseq2.R
+++ deseq2.R
@@ -1,28 +1,18 @@
-devtools::install_github("jbisanz/qiime2R")
-devtools::install_github("bryandmartin/corncob")
-BiocManager::install("DESeq2")
-library(Hmisc)
-# read_qza() - Function for reading artifacts (.qza).
-# qza_to_phyloseq() - Imports multiple artifacts to produce a phyloseq object.
-# read_q2metadata() - Reads qiime2 metadata file (containing q2-types definition line)
-# write_q2manifest() - Writes a read manifest file to import data into qiime2
-# theme_q2r() - A ggplot2 theme for publication-type figures.
-# print_provenance() - A function to display provenance information.
-# is_q2metadata() - A function to check if a file is a qiime2 metadata file.
-# parse_taxonomy() - A function to parse taxonomy strings and return a table where each column is a taxonomic class.
-# parse_ordination() - A function to parse the internal ordination format.
-# read_q2biom - A function for reading QIIME2 biom files in format v2.1
-# make_clr - Transform feature table using centered log2 ratio.
-# make_proportion - Transform feature table to proportion (sum to 1).
-# make_percent - Transform feature to percent (sum to 100).
-# interactive_table - Create an interactive table in Rstudio viewer or rmarkdown html.
-# summarize_taxa- Create a list of tables with abundances sumed to each taxonomic level.
-# taxa_barplot - Create a stacked barplot using ggplot2.
-# taxa_heatmap - Create a heatmap of taxonomic abundances using gplot2.
+# devtools::install_github("jbisanz/qiime2R")
+# devtools::install_github("bryandmartin/corncob")
+# BiocManager::install("DESeq2")
 
+##############################
+### PREPARE DATA FOR DESEQ ###
+
+library(Hmisc)
+source('functions.R')
+
+### 'formulas' element has actual formula in the name and contrasts as list elements. Variable of interest needs to be the last element of the formula
 ### !!! we should probably also analyze taxonomic tables?
 DESeq_analysis <- list('freq_dir' = '/home/adrian/Desktop/nx_temp/nx_data_q/',
-                       'freq_patt' = '^filtered.*FeatureTable.*',
+                       'freq_asv_patt' = '^filtered.*FeatureTable.*',
+                       'freq_tax_patt' = '^lev_.*FeatureTable.*',
                        'tax_dir' = '/home/adrian/Desktop/nx_temp/nx_taxonomy/',
                        'tax_patt' = '^vsearch.*Taxonomy\\.qza$',
                        'metadata' = qiime2R::read_q2metadata(file = '../qiime_metadata_nf.tsv'),
@@ -32,61 +22,77 @@ DESeq_analysis <- list('freq_dir' = '/home/adrian/Desktop/nx_temp/nx_data_q/',
                            's_f_l_o' = c('group', 'siderai_fe_liposomal_oral', 'anemia'))),
                        'seq_col_name' = 'seqs')
 
-DESeq_analysis$freq_files = list.files(path = DESeq_analysis$freq_dir, pattern = DESeq_analysis$freq_patt)
+
+
+### PREPARE MERGED TAXONOMY
 DESeq_analysis$tax_files = list.files(path = DESeq_analysis$tax_dir, pattern = DESeq_analysis$tax_patt)
 
 DESeq_analysis$tax_merged <- prepare_merged_taxonomies(list_tax_files = DESeq_analysis$tax_files, str_tax_dir = DESeq_analysis$tax_dir, seq_col_name_ = DESeq_analysis$seq_col_name)
 
+
+
+### PREPARE METADATA
 rownames(DESeq_analysis$metadata) <- as.character(DESeq_analysis$metadata[[1]])
 
+### All categorical variables should be factors I think! Control group needs to be the first factor. This can be set using relevel ###
+DESeq_analysis$metadata$group <- relevel(DESeq_analysis$metadata$group, ref = "anemia")
 
 
 
+### READ DATA FILES
+DESeq_analysis$freq_files = list.files(path = DESeq_analysis$freq_dir, pattern = paste0(DESeq_analysis$freq_asv_patt, '|', DESeq_analysis$freq_tax_patt))
 
 DESeq_analysis$freq <- purrr::map(
   .x = DESeq_analysis$freq_files, 
   .f = function(x){
+    
     qiime2R::read_qza(paste0(DESeq_analysis$freq_dir, x))
   })
 names(DESeq_analysis$freq) <- stringr::str_remove(string = DESeq_analysis$freq_files, pattern = '_FeatureTable.*')
 
 
 
+
+
+
 DESeq_analysis$freq_reorder <- purrr::map(
   .x = DESeq_analysis$freq, 
   .f = function(freq_data){
-    new_order <- as.data.frame(freq_data$data[, rownames(DESeq_analysis$metadata)])
+    
+    as.data.frame(freq_data$data[, rownames(DESeq_analysis$metadata)])
   })
-names(DESeq_analysis$freq_reorder) <- names(DESeq_analysis$freq)
+
+### PREPARE DATA FOR DESEQ ###
+##############################
 
 
 
 
+
+#############
+### DESEQ ###
 
 DESeq_analysis$deseq_data <- purrr::map(
   .x = DESeq_analysis$freq_reorder,
   .f = function(freq_data){
 
+    
+    
     temp <- purrr::map(
       .x = names(DESeq_analysis$formulas),
       .f = function(formula_){
         
         if (all(colnames(freq_data) == rownames(DESeq_analysis$metadata))) {
-          formula_dataset <- DESeq2::DESeqDataSetFromMatrix(
+          DESeq2::DESeqDataSetFromMatrix(
             countData = freq_data,
             colData = DESeq_analysis$metadata,
             design = as.formula(formula_) )
-
         }
       })
     names(temp) <- as.character(names(DESeq_analysis$formulas))
 
     return(temp)
   })
-names(DESeq_analysis$deseq_data) <- names(DESeq_analysis$freq)
-
-
-
 
 
 
@@ -102,7 +108,7 @@ DESeq_analysis$analysis <- purrr::map2(
     
     
     
-    formulas_ <- purrr::pmap(
+    purrr::pmap(
       .l = list(dataset, names(dataset), DESeq_analysis$formulas, names(DESeq_analysis$formulas)),
       .f = function(formula_dataset, formula_dataset_name, formula_expected, formula_expected_name) {
         DESeq_ <- DESeq2::DESeq(formula_dataset)
@@ -128,64 +134,150 @@ DESeq_analysis$analysis <- purrr::map2(
         
         return(list('DESeq' = DESeq_, 'contrasts' = contrasts))
       })
-    
-    names(formulas_) <- as.character(names(DESeq_analysis$formulas))
-    
-    return(formulas_)
   })
 
+### DESEQ ###
+#############
 
 
 
 
 
 
+#############################
+### ASV CENTERED ANALYSIS ###
+
+DESeq_analysis$asv_centered$analysis <- DESeq_analysis$analysis[stringr::str_detect(string = names(DESeq_analysis$analysis), pattern = DESeq_analysis$freq_asv_patt)]
 
 
 
-DESeq_analysis$merged_group_data <- purrr::map2(
+DESeq_analysis$asv_centered$merged_group_data <- purrr::map2(
   .x = DESeq_analysis$formulas,
   .y = names(DESeq_analysis$formulas),
   .f = function(formula, formula_name){
-    
-    
-    
+
+
     formulas_<- purrr::map2(
       .x = formula,
       .y = names(formula),
       .f = function(contrast, contrast_names){
-        
-        
-        
+
+
         contrasts_ <- purrr::map(
-          .x = DESeq_analysis$analysis,
+          .x = DESeq_analysis$asv_centered$analysis,
           .f = function(analysis){
-            
+
             contrast_ <- analysis[[formula_name]]$contrasts[[contrast_names]]$results_df
-            
+
             contrast_ <- subset(x = contrast_, subset = contrast_$pvalue < 0.05 )
           })
-        
+
         contrasts_ <- rlist::list.rbind(contrasts_)
-    })
-})    
+
+        contrasts_ <- wrapper_get_all_values_for_seqences(
+          list_of_original_datasets = DESeq_analysis$freq_reorder,
+          df_with_sequences = contrasts_,
+          df_with_sequences_sequence_colname = 'seqs')
+        contrasts_ <- unique(dplyr::select(contrasts_, -dataset))
+
+        return(contrasts_)
+      })
+  })
 
 
 
-
-
-
-
-
-DESeq_analysis$tax_merged_names <- tidyr::unite(data = DESeq_analysis$tax_merged, 1:7, col = name, sep = '_X_')
-
-DESeq_analysis$taxonomic_data_for_visualization <- purrr::map2(
-  .x = DESeq_analysis$freq_reorder,
-  .y = names(DESeq_analysis$freq_reorder),
-  .f = function(dataset, dataset_name){
+DESeq_analysis$asv_centered$taxonomic_data_for_visualization <- purrr::map2(
+  .x = DESeq_analysis$asv_centered$merged_group_data,
+  .y = names(DESeq_analysis$asv_centered$merged_group_data),
+  .f = function(variable, variable_name){
     
 
-})    
+    purrr::map2(
+      .x = variable,
+      .y = names(variable),
+      .f = function(contrast_, contrast_name){
+        
+        contrast_$full_name <- NA
+        
+        for (row in seq_along(contrast_[[1]])) {
+          contrast_$full_name[[row]] <- paste(contrast_[row,8:14], collapse = '__')
+        }
+        
+        return( dplyr::select(contrast_, 1, 18:27) )
+      })    
+  }) 
+
+### ASV CENTERED ANALYSIS ###
+#############################
+
+
+
+
+
+
+
+#############################
+### TAX CENTERED ANALYSIS ###
+
+DESeq_analysis$tax_centered <- purrr::map(
+  .x = seq(2, 7),
+  .f = function(level){
+    
+    tax_centered_analysis <- DESeq_analysis$analysis[stringr::str_detect(string = names(DESeq_analysis$analysis), pattern = DESeq_analysis$freq_tax_patt)]
+    
+    tax_centered_analysis[stringr::str_detect(
+      string = names(tax_centered_analysis), 
+      pattern = paste0('^lev_', as.character(level)))]
+    
+  })
+names(DESeq_analysis$tax_centered) <- as.character(seq(2, 7))
+
+
+
+
+
+
+DESeq_analysis$tax_centered$merged_group_data <- purrr::map2(
+  .x = DESeq_analysis$formulas,
+  .y = names(DESeq_analysis$formulas),
+  .f = function(formula, formula_name){
+
+    
+    purrr::map2(
+      .x = formula,
+      .y = names(formula),
+      .f = function(contrast, contrast_names){
+       
+         
+        purrr::map2(
+          .x = DESeq_analysis$tax_centered,
+          .y = names(DESeq_analysis$tax_centered),
+          .f = function(tax_lev, tax_lev_name){ 
+            
+            
+            
+            contrasts_ <- purrr::map(
+              .x = tax_lev,
+              .f = function(analysis){
+                
+                contrast_ <- analysis[[formula_name]]$contrasts[[contrast_names]]$results_df
+                
+                contrast_ <- subset(x = contrast_, subset = contrast_$pvalue < 0.05 )
+              })
+            
+            contrasts_ <- rlist::list.rbind(contrasts_)
+            
+            if (!is.null(contrasts_)) {
+              contrasts_ <- contrasts_[,-c(8:15)]
+            }
+            
+            return(contrasts_)
+    })
+  })
+})
+
+### TAX CENTERED ANALYSIS ###
+#############################
 
 
 
@@ -195,4 +287,4 @@ DESeq_analysis$taxonomic_data_for_visualization <- purrr::map2(
 
 
 
-
+# plotCounts - for extracting single genes for visualization (ggplot) i think?
--- functions.R
+++ functions.R
@@ -1,3 +1,5 @@
+source('https://raw.githubusercontent.com/AdrianS85/helper_R_functions/master/little_helpers.R')
+
 prepare_merged_taxonomies <- function(list_tax_files, str_tax_dir, seq_col_name_)
 {
   taxonomy <- purrr::map(
@@ -16,3 +18,48 @@ prepare_merged_taxonomies <- function(list_tax_files, str_tax_dir, seq_col_name_
 
   return(taxonomy)
 }
+
+
+
+
+
+
+wrapper_get_all_values_for_seqences <- function(list_of_original_datasets, df_with_sequences, df_with_sequences_sequence_colname, collapse_values_from_dataset_using_median = T)
+{
+  assertthat::assert_that(are_vectors_the_same(purrr::map(.x = list_of_original_datasets, .f = colnames))) ### !!! check this
+
+  list_of_original_datasets_with_names <- purrr::map2(
+    .x = list_of_original_datasets,
+    .y = names(list_of_original_datasets),
+    .f = function(dataset_, dataset_name) {
+      
+      if (collapse_values_from_dataset_using_median == F) {
+        dataset_$dataset_of_origin <- stringr::str_remove(dataset_name, '\\..*')
+      }
+      
+      dataset_[[df_with_sequences_sequence_colname]] <- stringr::str_remove_all(rownames(dataset_), '.*\\.')
+
+      return(dataset_)
+    })
+  
+  df_with_values_for_all_seqs_from_all_datasets <- rlist::list.rbind(list_of_original_datasets_with_names)
+  
+  if (collapse_values_from_dataset_using_median == T) {
+    warning('BEWARE! Values from all datasets in list_of_original_datasets are medianed! This is ok for e.g. filtered data for deblur, deblur with singletons and dada2, but that may not be so every time')
+    
+    df_with_values_for_all_seqs_from_all_datasets <- group_and_then_summarize_all_cols_in_df(
+      df_ = df_with_values_for_all_seqs_from_all_datasets, 
+      group_by_col = df_with_sequences_sequence_colname, 
+      summarize_method = 'median')
+    }
+
+  df_with_values_for_all_seqs_from_all_datasets <- merge(
+    x = df_with_sequences, 
+    y = df_with_values_for_all_seqs_from_all_datasets,
+    by = df_with_sequences_sequence_colname,
+    all.x = T)
+
+  return(df_with_values_for_all_seqs_from_all_datasets)
+}
+
+
